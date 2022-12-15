b6234606a53844a053ccd2c0eabbdb253f23923b
romluc <romulofilhofilho@gmail.com>
2022-12-14 21:26:44 -0500

    Day 8 - Pt.2

--- day8.js
+++ day8.js
@@ -1,24 +1,25 @@
 const {
   isGreaterThanOthersInLine,
   howManyUntilEqualOrTaller,
-} = require('./utils');
-const fs = require('fs');
-fs.readFile('inputs/input8_1.txt', 'utf8', (err, data) => {
+} = require('./utils')
+const fs = require('fs')
+const { listeners } = require('process')
+fs.readFile('inputs/input8.txt', 'utf8', (err, data) => {
   if (err) {
-    throw err;
+    throw err
   }
 
-  const FIRST_LINE = 99;
-  const LAST_LINE = 99;
-  const TWO_LATERAL_COLUMNS = 97 * 2;
-  const edgesTrees = FIRST_LINE + LAST_LINE + TWO_LATERAL_COLUMNS;
+  const FIRST_LINE = 99
+  const LAST_LINE = 99
+  const TWO_LATERAL_COLUMNS = 97 * 2
+  const edgesTrees = FIRST_LINE + LAST_LINE + TWO_LATERAL_COLUMNS
 
-  const matrix = data.split(/\r?\n/);
-  const treeMatrix = matrix.map(toArray);
+  const matrix = data.split(/\r?\n/)
+  const treeMatrix = matrix.map(toNumbersArray)
 
-  let arrayOfItemsEqualOrTaller = [];
-  let number = 0;
-  let sum = 0;
+  let arrayOfItemsEqualOrTaller = []
+  let number = 0
+  let sum = 0
 
   // Pt.1
   // for (let i = 1; i < treeMatrix.length - 1; i++) {
@@ -28,17 +29,46 @@ fs.readFile('inputs/input8_1.txt', 'utf8', (err, data) => {
   // }
 
   // Pt.2
-  for (let i = 1; i < treeMatrix.length - 1; i++) {
-    for (let j = 1; j < treeMatrix.length - 1; j++) {
-      arrayOfItemsEqualOrTaller.push(
-        howManyUntilEqualOrTaller(treeMatrix, i, j)
-      );
+  function checkEqualOrTallerInLine(y, x, dy, dx, map) {
+    let visible = 0
+    let maximum = map[y][x]
+
+    while (true) {
+      y += dy
+      x += dx
+      map[y]
+
+      if (y < 0 || y >= map.length || x < 0 || x >= map[y].length) {
+        break
+      }
+      visible++
+
+      if (map[y][x] >= maximum) {
+        break
+      }
     }
+    return visible
   }
-  console.log(arrayOfItemsEqualOrTaller);
-  console.log(Math.max(...arrayOfItemsEqualOrTaller));
+
+  function pt2() {
+    let max = 0
+    for (let y = 0; y < treeMatrix.length; y++) {
+      for (let x = 0; x < treeMatrix[y].length; x++) {
+        const scenicScore =
+          checkEqualOrTallerInLine(y, x, -1, 0, treeMatrix) *
+          checkEqualOrTallerInLine(y, x, 1, 0, treeMatrix) *
+          checkEqualOrTallerInLine(y, x, 0, 1, treeMatrix) *
+          checkEqualOrTallerInLine(y, x, 0, -1, treeMatrix)
+        if (scenicScore > max) max = scenicScore
+      }
+    }
+    console.log(max)
+  }
+
+  pt2()
+  // console.log(arrayOfItemsEqualOrTaller);
 
   // console.log(sum + edgesTrees);
-});
+})
 
-const toArray = (line) => Array.from(line);
+const toNumbersArray = (line) => Array.from(line).map(Number)
